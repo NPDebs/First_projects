ccd627fbacce92c715eed28a01bc8d53f952e539
NPDebs <105166953+NPDebs@users.noreply.github.com>
2022-05-28 23:46:57 +0100

    Add files via upload

---
+++ classes-and-objects-1.py
@@ -0,0 +1,33 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+    
+    print("Student Information:")
+
+    def change_name(self, newName):
+        self.name = newName
+        print("Name: ", self.name)
+
+    def change_age(self, newAge):
+        self.age = newAge
+        print("Age: ", self.age)
+
+    def add_track(self, newTrack):
+        self.tracks = newTrack
+        print("Track: ", self.tracks)
+
+    def get_score(self, newScore):
+        self.score = newScore
+        print("Score: ", self.score)
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Kate")
+Bob.change_age(48)
+Bob.add_track(["FE","BE","UI/UX"])
+Bob.get_score(72.30)
